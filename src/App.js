404af93901959d7e9ebce1bcfcc512a42c5e2751
Dan McKinney <3dhdaniel@gmail.com>
2019-08-13 20:49:12 -0400

    first and only

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios'
+
+
+export default function App() {
+  const [result, setResults] = useState([])
+
+  useEffect(() => {
+    getResult()
+    
+    
+  }, [])
+
+  const getResult = async () => {
+    const res = await axios.get('http://hn.algolia.com/api/v1/search?query=reacthooks')
+    setResults(res.data.hits)
+  }
+
+  return (
+    <>
+    <ul>
+      {result.map(result => (
+        <li key={result.objectID}>
+        <a href={result.url}>{result.title}</a>
+      </li>
+      ))}
+    </ul>
+    
+    </>
+  )
+}
